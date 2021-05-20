09d8bf2735425bfead351ba1c45125f6d75bc91c
jobdk <johnybudnik+git@gmail.com>
2021-05-20 20:52:25 +0200

    fixed cors policy

--- src/main/java/com/example/configbackendmockserver/ConfigbackendmockserverApplication.java
+++ src/main/java/com/example/configbackendmockserver/ConfigbackendmockserverApplication.java
@@ -3,11 +3,12 @@ package com.example.configbackendmockserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ConfigbackendmockserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigbackendmockserverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigbackendmockserverApplication.class, args);
+    }
 
 }
--- src/main/java/com/example/configbackendmockserver/controller/StubController.java
+++ src/main/java/com/example/configbackendmockserver/controller/StubController.java
@@ -1,5 +1,6 @@
 package com.example.configbackendmockserver.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,12 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
+
 @RequestMapping("config_backend")
 @RestController()
 public class StubController {
+
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("apisettings")
     public String getApiSetting() throws IOException {
         File jsonFile = new File("../configbackendmockserver/src/main/resources/apisettings.json");
